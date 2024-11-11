922da6d861b0ee9033c69d8de903331b38f9e057
mario <mdlnxd@hotmail.com>
2024-11-11 16:44:15 -0300

    add modal alert

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,20 @@ function App() {
   const [cidade, setCidade] = useState('');
   const [dados, setDados] = useState();
   const [dados5dias, setDados5dias] = useState();
-
-  const apikey = '3b0998770f55ec57793c03c92e408c0b'
+  const [msg, setMsg] = useState('');
+  const [openModal, setOpenModal] = useState(false);
+   const apikey = '3b0998770f55ec57793c03c92e408c0b'
 
   async function buscarCidade(){
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apikey}&units=metric&lang=pt_br`
+    if (!cidade){
+      setOpenModal(true);
+      setMsg('Informe o nome da cidade!')
+      return;
+    }
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apikey}&units=metric&lang=pt_br`
     const data = await axios.get(url)
-    
+
     const url2 = `https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=${apikey}&units=metric&lang=pt_br`
     const data2 = await axios.get(url2);
 
@@ -23,10 +29,18 @@ function App() {
     setDados5dias(data2.data)
     console.log(data.data)
     console.log(data2.data)
+
+    
+    } catch (error) {
+      setOpenModal(true);
+      setMsg('Cidade não localizada!')
+      return;
+    }
+    
   }
 
   return (
-   <div className='div1'>
+    <div className='div1'>
     <h1 className='txtTitulo'>Previsão do Tempo</h1>
     <div>
     <input className='input'
@@ -47,6 +61,47 @@ function App() {
       <InformacoesDetalhadas cfg = {dados5dias}/>
     )}
 
+    {msg && (
+      <modal
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 1000,
+      }}
+      onClick={()=>setMsg('')}
+      >
+        <div
+        style={{
+          backgroundColor: '#fff',
+          padding: '20px',
+          width: '90%',
+          maxWidth: '100px',
+          borderRadius: '8px',
+          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+          position: 'relative',
+        }}
+        >
+          <p>{msg}</p>
+          <button
+          style={{
+            marginTop:10,
+            backgroundColor:'gray',
+            padding:4,
+            color:'white',
+            borderRadius:5,
+            border:'none'
+          }}
+          >Ok</button>
+        </div>
+      </modal>
+    )}
    </div>
   )
 }
